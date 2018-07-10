4ae4aff4fabbf60f0169e50fd5f425402ff13e50
Rimon Mostafiz <ferary.rimon@gmail.com>
2018-07-11 00:59:19 +0600

    Update request url

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const request = require('request');
 
+var url = 'https://maps.google.com/maps/api/geocode/json?address=house21%road28%20banani%20model%20town%20dhaka';
+
 request({
-    url: 'https://maps.google.com/maps/api/geocode/json?address=house21%road28%20banani%20model%20town%20dhaka',
+    url: url,
     json: true
 }, (error, response, body) => {
     console.log(body);
